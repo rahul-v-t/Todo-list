b7b47e20fcb89be86d47b58cac47941f9dd7a76a
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-10-04 10:47:00 +0530

    Todo Upadted

--- src/components/screens/Todo.js
+++ src/components/screens/Todo.js
@@ -5,15 +5,33 @@ class Todo extends React.Component {
         super(props)
         this.state = {
             items:[],
+            input :"",
         };
     }
+    removeItem = (id) =>{
+        console.log(id);
+        let new_items = this.state.items.filter((item)=> item.id !== id);
+        this.setState({ items: new_items });
+    };
     renderitems = () =>{
-        this.state.items.map((item) =>{
-            return(
-                <li key={item.id}>{item.title} <button>Delete</button> </li>
-            );
-        });
-    }
+        return this.state.items.map((item) => (
+               <li key={item.id}>{item.title} <button onClick = { () => this.removeItem(item.id)} >Delete</button> </li>
+        ));
+    };
+
+    upadateItem = () =>{
+        let new_item = {
+            id : this.state.items.length,
+            title : this.state.input,
+        };
+        if(this.state.input){
+            this.setState({
+                items:[...this.state.items, new_item],
+                input:"",
+            });
+        }
+    };
+
     render(){
         return(
             <>
@@ -21,8 +39,10 @@ class Todo extends React.Component {
                 <ul>
                     {this.renderitems()}
                 </ul>
-                <input placeholder="New Item" />
-                <button>Add New</button>
+                <input placeholder="New Item"
+                 value={this.state.input}
+                 onChange={(e) => this.setState({input : e.target.value })} />
+                <button onClick={this.upadateItem}>Add New </button>
            </>
         );
     }
